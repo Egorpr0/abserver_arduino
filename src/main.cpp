af1d913ab3508968c095f9049ec85199b38a0b6d
Egor Khanko <Beluerosu52@gmail.com>
2020-12-13 00:23:58 +0300

    Improve string parsing algorythm. Change baud rate

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ void setup()
   Timer2.setPeriod(16384);
   Timer2.enableISR();
 
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(13, OUTPUT);
 }
 
@@ -34,25 +34,6 @@ ISR(TIMER2_A)
     pingTimer = millis();
     Serial.println("ping " + String(millis()));
   }
-}
-
-Vector<String> divideString(String recievedString)
-{
-  String storage_array[10];
-  Vector<String> vector(storage_array);
-  vector.push_back("");
-  for (int i = 0; i < recievedString.length(); i++)
-  {
-    if (recievedString[i] == ' ')
-    {
-      vector.push_back("");
-    }
-    else
-    {
-      vector.back() += recievedString[i];
-    }
-  }
-  return vector;
 }
 
 void blinkLed(int numberOfBlinks)
@@ -70,46 +51,36 @@ void blinkLed(int numberOfBlinks)
 //blink [number of blinks] - only for test purposes! Blink led on pin 13, made with delay, so unstable
 //rotate [to] - rotate to some point in decimal degrees
 //ping - returns pong to serial port
-void analyzeString(Vector<String> data)
-{
-  char firstOption = data[0][0];
-  switch (firstOption)
-  {
-  case 'b':
-  {
-    blinkLed(data[1].toInt());
-  }
-  case 'p':
-  {
-    Serial.println("pong");
-  }
-
-  break;
-
-  default:
-    break;
-  }
-}
+//void analyzeString(String data)
+//{
+//  char firstOption = data[0][0];
+//  switch (firstOption)
+//  {
+//  case 'b':
+//  {
+//    blinkLed(data[1].toInt());
+//  }
+//  case 'p':
+//  {
+//    Serial.println("pong");
+//  }
+//
+//  break;
+//
+//  default:
+//    break;
+//  }
+//}
 
 void loop()
 {
   if (Serial.available())
   {
-    String s = Serial.readStringUntil('\n');
-    String storage_array[10];
-    Vector<String> recievedString(storage_array);
-    recievedString.push_back("");
-    for (int i = 0; i < s.length(); i++)
-    {
-      if (s[i] == ' ')
-      {
-        recievedString.push_back("");
-      }
-      else
-      {
-        recievedString.back() += s[i];
-      }
-    }
-    analyzeString(recievedString);
+    String recievedString = Serial.readStringUntil('\n');
+    char convertedString[recievedString.length()];
+    recievedString.toCharArray(convertedString, recievedString.length());
+    char *dividedString = strtok(convertedString, " ");
+
+    //analyzeString(recievedString);
   }
 }
