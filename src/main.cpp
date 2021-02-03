273a17bc2c1b088da0a3094cea1facfe41ea3ad0
Egor Khanko <beluerosu52@gmail.com>
2021-02-03 21:00:53 +0300

    add gyverstepper library

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
+#include "Dump.h"
+#include "GyverStepper.h"
+#include "GyverTimers.h"
 #include <Arduino.h>
 #include <Vector.h>
-#include "GyverTimers.h"
-#include "Dump.h"
 
-//global storage variables
+// steppers
+GStepper<STEPPER2WIRE> stepper1(200, 4, 5);
+GStepper<STEPPER2WIRE> stepper2(200, 5, 6);
+
+// global storage variables
 uint32_t pingTimer;
-//print/return avalile memory
-int availableMemory()
-{
+
+// print/return avalile memory
+int availableMemory() {
   int size = 2048;
   byte *buf;
   while ((buf = (byte *)malloc(--size)) == NULL)
@@ -17,30 +22,30 @@ int availableMemory()
   return size;
 };
 
-void setup()
-{
-  //timers setup
-  //timer for outputting to serial and status reporting
-  Timer2.setPeriod(16384);
-  Timer2.enableISR();
+void setup() {
+  stepper1.setRunMode(KEEP_SPEED);
+  stepper1.setSpeed(3200);
+  stepper1.setMaxSpeed(3200);
+  stepper1.setAcceleration(500);
+  // timers setup
+  // timer for outputting to serial and status reporting
+  // Timer2.setPeriod(16384);
+  // Timer2.enableISR();
 
   Serial.begin(115200);
   pinMode(13, OUTPUT);
 }
 
-ISR(TIMER2_A)
-{
-  //if (millis() - pingTimer >= 2000)
+ISR(TIMER2_A) {
+  // if (millis() - pingTimer >= 2000)
   //{
   //  pingTimer = millis();
   //  Serial.println("ping " + String(millis()));
   //}
 }
 
-void blinkLed(int numberOfBlinks)
-{
-  for (int i = 0; i < numberOfBlinks; i++)
-  {
+void blinkLed(int numberOfBlinks) {
+  for (int i = 0; i < numberOfBlinks; i++) {
     digitalWrite(13, HIGH);
     delay(50);
     digitalWrite(13, LOW);
@@ -48,31 +53,23 @@ void blinkLed(int numberOfBlinks)
   }
 }
 
-//analyzing first letter of string because of switch limitations
-//blink [number of blinks] - only for test purposes! Blink led on pin 13, made with delay, so unstable
-//rotate [to] - rotate to some point in decimal degrees
-//ping - returns pong to serial port
-void analyzeString(String data[])
-{
-  if (data[0] == "rotate")
-  {
+// blink [number of blinks] - only for test purposes! Blink led on pin 13, made with delay, so unstable
+// rotate [to] - rotate to some point in decimal degrees
+// ping - returns pong to serial port
+void doAction(String data[]) {
+  if (data[0] == "rotate") {
     Serial.println("rotate");
-  }
-  else if (data[0] == "ping")
-  {
+  } else if (data[0] == "ping") {
     Serial.println("pong");
-  }
-  else if (data[0] == "blink")
-  {
+  } else if (data[0] == "blink") {
     blinkLed(data[1].toInt());
   }
 }
 
-void loop()
-{
-  if (Serial.available())
-  {
-    //recieveing string and parsing into strings array
+void loop() {
+  if (Serial.available()) {
+
+    // recieveing string and parsing into strings array
     String recievedString = Serial.readStringUntil('\n');
     uint32_t recievedStringLength = recievedString.length();
     char convertedString[recievedStringLength];
@@ -80,12 +77,12 @@ void loop()
     char *dividedString = strtok(convertedString, " ");
 
     String parsedString[recievedStringLength];
-    while (dividedString != NULL)
-    {
+    while (dividedString != NULL) {
       Serial.println(dividedString);
       dividedString = strtok(NULL, " ");
     }
 
-    analyzeString(parsedString);
+    doAction(parsedString);
   }
+  stepper1.tick();
 }
